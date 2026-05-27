fe5e0117312f5674d93d4e1ff0f44a6568990ebf
Davlee1 <davidlee21019@gmail.com>
2026-05-27 19:16:16 -0400

    fixed button bug

--- frontend/src/components/features/RegisterForm.tsx
+++ frontend/src/components/features/RegisterForm.tsx
@@ -1,65 +1,74 @@
 import TextInputWithLabel from '../shared/TextInputWithLabel.tsx'; //from login pr
 import { useState } from 'react';
 
-interface LoginFormProps {
+interface RegisterFormProps {
   onRegister: (data: { name: string; email: string; password: string }) => void;
 }
 
-const RegisterForm = function ({ onRegister }: LoginFormProps) {
+const RegisterForm = function ({ onRegister }: RegisterFormProps) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
 
   const handleRegister = () => {
     if (password === confirmPassword) {
       onRegister({ name, email, password });
     } else {
-      // add error message here
+      setError("Passwords do not match.")
     }
   };
 
   return (
-    
-      <form>
-        <TextInputWithLabel
-          elementId="name"
-          label="Name"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setName(e.currentTarget.value)
-          }
-          value={name}
-        />
-        <TextInputWithLabel
-          elementId="email"
-          label="Email"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setEmail(e.currentTarget.value)
-          }
-          value={email}
-        />
-        <TextInputWithLabel
-          elementId="password"
-          label="Password"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setPassword(e.currentTarget.value)
-          }
-          value={password}
-          type="password"
-        />
-        <TextInputWithLabel
-          elementId="confirm_password"
-          label="Confirm Password"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setConfirmPassword(e.currentTarget.value)
-          }
-          value={confirmPassword}
-          type="password"
-        />
+    <>
+    if(error != ''){
+      <div id="error">{error}</div>
+    }
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleRegister();
+      }}
+    >
+      <TextInputWithLabel
+        elementId="name"
+        label="Name"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setName(e.currentTarget.value)
+        }
+        value={name}
+      />
+      <TextInputWithLabel
+        elementId="email"
+        label="Email"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setEmail(e.currentTarget.value)
+        }
+        value={email}
+      />
+      <TextInputWithLabel
+        elementId="password"
+        label="Password"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setPassword(e.currentTarget.value)
+        }
+        value={password}
+        type="password"
+      />
+      <TextInputWithLabel
+        elementId="confirm_password"
+        label="Confirm Password"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setConfirmPassword(e.currentTarget.value)
+        }
+        value={confirmPassword}
+        type="password"
+      />
 
-        <button onClick={handleRegister}>Submit</button>
-      </form>
-    
+      <button type="submit">Submit</button>
+    </form>
+    </>
   );
 };
 
