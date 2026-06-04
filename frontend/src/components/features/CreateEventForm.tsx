db4f4282725a628f9493424a9165c04b42644300
Davlee1 <davidlee21019@gmail.com>
2026-06-04 12:12:58 -0400

    ptogress on create event form

--- frontend/src/components/features/CreateEventForm.tsx
+++ frontend/src/components/features/CreateEventForm.tsx
@@ -1,24 +1,26 @@
 import TextInputWithLabel from '../shared/TextInputWithLabel.tsx'; //from login pr
 import { useState } from 'react';
-import { Link } from "react-router";
+import { Link } from 'react-router';
 
 interface CreateEventFormProps {
-  onCreateEvent: (data: { name: string; email: string; password: string }) => void;
+  onCreateEvent: (
+    group_id: string,
+    title: string,
+    description: string,
+    event_date: string,
+    status: 'planned' | 'completed' | 'cancelled',
+  ) => void;
 }
 
 const CreateEventForm = function ({ onCreateEvent }: CreateEventFormProps) {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [error, setError] = useState('');
+  //const [group_id, setGroup_id] = useState('10');
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [event_date, setEvent_date] = useState('');
+  //const [status, setStatus] = useState<'planned' | 'completed' | 'cancelled'>('planned');
 
   const handleCreateEvent = () => {
-    if (password === confirmPassword) {
-      onCreateEvent({ name, email, password });
-    } else {
-      setError('Passwords do not match.');
-    }
+    onCreateEvent('10', title, description, event_date, 'planned');
   };
 
   const containerStyling =
@@ -28,7 +30,7 @@ const CreateEventForm = function ({ onCreateEvent }: CreateEventFormProps) {
 
   return (
     <div className={containerStyling}>
-      <h1 className="text-xl pt-3 pb-2">Register</h1>
+      <h1 className="text-xl pt-3 pb-2">Create Event</h1>
       <hr className="pb-5 text-indigo-500/50" />
       <form
         onSubmit={(e) => {
@@ -37,47 +39,40 @@ const CreateEventForm = function ({ onCreateEvent }: CreateEventFormProps) {
         }}
       >
         <TextInputWithLabel
-          elementId="name"
-          label="Name"
+          elementId="title"
+          label="Title"
           onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setName(e.currentTarget.value)
+            setTitle(e.currentTarget.value)
           }
-          value={name}
+          value={title}
         />
         <TextInputWithLabel
-          elementId="email"
-          label="Email"
+          elementId="description"
+          label="Description"
           onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setEmail(e.currentTarget.value)
+            setDescription(e.currentTarget.value)
           }
-          value={email}
+          value={description}
         />
-        <TextInputWithLabel
-          elementId="password"
-          label="Password"
+
+        <label>Date:</label>
+        <input
+          type="datetime-local"
+          id="date"
+          name="date"
           onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setPassword(e.currentTarget.value)
+            setEvent_date(e.currentTarget.value)
           }
-          value={password}
-          type="password"
-        />
-        <TextInputWithLabel
-          elementId="confirm_password"
-          label="Confirm Password"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setConfirmPassword(e.currentTarget.value)
-          }
-          value={confirmPassword}
-          type="password"
-        />
-        <Link className={buttonStyling} to="/login">
-          Login Page
+          value={event_date}
+        ></input>
+
+        <Link className={buttonStyling} to="/dashboard">
+          Cancel
         </Link>
         <button className={buttonStyling} type="submit">
           Submit
         </button>
       </form>
-      <p id="error">{error}</p>
     </div>
   );
 };
