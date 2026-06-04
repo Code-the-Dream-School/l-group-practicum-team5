2bf07b22b5ab28c2f1fcbb45138fbe51ef553e16
Davlee1 <davidlee21019@gmail.com>
2026-06-04 01:33:00 -0400

    set up displaying events in the dashboard, started on create event form

--- frontend/src/Dashboard/DashboardLayout.tsx
+++ frontend/src/Dashboard/DashboardLayout.tsx
@@ -1,31 +1,40 @@
 import './dashboardlayout.css';
 import DashboardHeader from './DashboardHeader';
 import StatsGrid from './StatsGrid';
-import LastHangoutBanner from './LastHangoutBanner';
+import { Link } from 'react-router';
+import AllEvents from '../components/features/AllEvents.tsx';
 
 export default function DashboardLayout() {
+  const buttonStyling =
+    'bg-[#e4d1ff] hover:bg-white text-[#5200c5] p-2 mt-7px ml-5  text-center rounded-xl';
   return (
     <div className="dashboard-container">
       <DashboardHeader />
 
       <main className="dashboard-main">
-        <StatsGrid />
-
-        <LastHangoutBanner />
+        <div className="last-hangout-banner">
+          <StatsGrid />
+        </div>
 
         <div className="main-grid">
           <div className="left-column">
-            <div className="card">
-              <h2 className="card-title">Upcoming Events</h2>
+            <div className="card-events">
+              <Link className={buttonStyling} to="/create-event">
+                Create Event
+              </Link>
+              <h2 className="card-title">Events</h2>
+
+              <hr className="pb-3 text-white" />
               <div className="card-content">
-                <p>No events yet</p>
+                <AllEvents />
               </div>
             </div>
           </div>
 
           <div className="right-column">
-            <div className="card">
+            <div className="card-members">
               <h2 className="card-title">Group Members</h2>
+              <hr className="pb-3  text-white" />
               <div className="card-content">
                 <p>No members yet</p>
               </div>
