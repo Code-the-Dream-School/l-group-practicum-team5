1fb229da3cba3b106f721ef1d2e067aa253598cc
Davlee1 <davidlee21019@gmail.com>
2026-06-05 11:51:40 -0400

    fixed styling for createw event form

--- frontend/src/components/features/CreateEventForm.tsx
+++ frontend/src/components/features/CreateEventForm.tsx
@@ -9,24 +9,29 @@ interface CreateEventFormProps {
     description: string,
     event_date: string,
     status: 'planned' | 'completed' | 'cancelled',
-  ) => void;
+  ) => Promise<void>;
 }
 
 const CreateEventForm = function ({ onCreateEvent }: CreateEventFormProps) {
-  //const [group_id, setGroup_id] = useState('10');
+  const [group_id, setGroup_id] = useState('10');
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [event_date, setEvent_date] = useState('');
   //const [status, setStatus] = useState<'planned' | 'completed' | 'cancelled'>('planned');
 
   const handleCreateEvent = () => {
-    onCreateEvent('10', title, description, event_date, 'planned');
+    onCreateEvent(group_id, title, description, event_date, 'planned');
   };
 
   const containerStyling =
     'container m-auto mt-5 max-w-4/10 text-center bg-white pt-2 pb-2 pr-5 pl-5 rounded-4xl';
   const buttonStyling =
     'bg-[#5200c5] hover:bg-[#8340e0] text-white p-2 m-5 rounded-xl';
+  const inputStyling =
+    'bg-white border-2 border-indigo-500/50 hover:border-[#d1b1ff] mb-3 rounded-3xl p-1';
+
+  const descStyling =
+    'bg-white border-2 border-indigo-500/50 hover:border-[#d1b1ff] mb-3 rounded-3xl p-1 h-20 w-60';
 
   return (
     <div className={containerStyling}>
@@ -39,6 +44,14 @@ const CreateEventForm = function ({ onCreateEvent }: CreateEventFormProps) {
         }}
       >
         <TextInputWithLabel
+          elementId="group_id"
+          label="Group id"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setGroup_id(e.currentTarget.value)
+          }
+          value={group_id}
+        />
+        <TextInputWithLabel
           elementId="title"
           label="Title"
           onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
@@ -46,25 +59,35 @@ const CreateEventForm = function ({ onCreateEvent }: CreateEventFormProps) {
           }
           value={title}
         />
-        <TextInputWithLabel
-          elementId="description"
-          label="Description"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setDescription(e.currentTarget.value)
-          }
-          value={description}
-        />
 
-        <label>Date:</label>
-        <input
-          type="datetime-local"
-          id="date"
-          name="date"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setEvent_date(e.currentTarget.value)
-          }
-          value={event_date}
-        ></input>
+        <div>
+          <label>Description</label>
+          <br />
+          <textarea
+            className={descStyling}
+            id="description"
+            name="description"
+            onInput={(e) => setDescription(e.currentTarget.value)}
+            value={description}
+          ></textarea>
+          <br />
+        </div>
+
+        <div>
+          <label>Date and time</label>
+          <br />
+          <input
+            className={inputStyling}
+            type="datetime-local"
+            id="date"
+            name="date"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setEvent_date(e.currentTarget.value)
+            }
+            value={event_date}
+          ></input>
+          <br />
+        </div>
 
         <Link className={buttonStyling} to="/dashboard">
           Cancel
